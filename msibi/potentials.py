c5613520579b785a2d1678e3234d8eb7caaf7cb4
Christoph Klein <christoph.t.klein@me.com>
2015-03-23 14:23:15 -0500

    fix potentials

--- msibi/potentials.py
+++ msibi/potentials.py
@@ -5,18 +5,10 @@ from msibi.optimize import R
 
 def mie(eps, sig, m=12, n=6):
     """Mie pair potential. """
-    return 4 * eps * [(sig / R) ** m - (sig / R) ** n]
+    return 4 * eps * ((sig / R) ** m - (sig / R) ** n)
 
 
 def morse(D, alpha, r0):
     """Morse pair potential. """
-    return D * [np.exp(-2 * alpha * (R - r0)) -
-                2 * np.exp(-alpha * (R - r0))]
-
-
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-
-    y = mie(1.0, 1.0)
-    plt.plot(R, y)
-    plt.show()
+    return D * (np.exp(-2 * alpha * (R - r0)) -
+                2 * np.exp(-alpha * (R - r0)))
