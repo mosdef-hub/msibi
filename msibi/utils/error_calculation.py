75c56ea63e726af6dd9285ae27ce8ce52d6f29f4
Tim Moore <tcmoore3@gmail.com>
2015-03-25 21:38:07 -0500

    add error calculation

---
+++ msibi/utils/error_calculation.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+def calc_similarity(arr1, arr2):
+    f_fit = np.sum(np.absolute(arr1 - arr2))
+    f_fit /= np.sum((np.absolute(arr1) + np.absolute(arr2)))
+    return 1.0 - f_fit
