331239ecbf638a7732305d4c1fda288ae9fc82a9
Tim Moore <tcmoore3@gmail.com>
2015-03-30 18:13:22 -0500

    smoothing functions

---
+++ msibi/utils/smoothing.py
@@ -0,0 +1,40 @@
+import numpy as np
+from math import factorial
+def savitzky_golay(y, window_size, order, deriv=0, rate=1):
+    try: 
+        window_size = np.abs(np.int(window_size))
+        order = np.abs(np.int(order))
+    except ValueError, msg:
+        raise ValueError("window_size and order have to be of type int")
+    if window_size % 2 != 1 or window_size < 1:
+        raise TypeError('window_size must be a positive odd number')
+    if window_size < order + 2:
+        raise TypeError('window_size is too small for the polynomials order')
+    order_range = range(order+1)
+    half_window = (window_size - 1) // 2
+    b = np.mat([[k**i for i in order_range] for k in range(-half_window,
+        half_window+1)])
+    m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
+    firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
+    lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
+    y = np.concatenate((firstvals, y, lastvals))
+    return np.convolve(m[::-1], y, mode='valid')
+
+def smooth(x, window_len, window='blackman'):
+    if x.ndim != 1:
+        raise ValueError('smooth only accepts 1 dimension arrays.')
+    if x.size < window_len:
+        raise ValueError('Input vector needs to be bigger than window size.')
+    if window_len < 3:
+        return x
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError('Unsupported smoothing window type')
+    s = np.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
+    if window == 'flat':
+        w = np.ones(window_len, 'd')
+    else:
+        w = eval('np.'+window+'(window_len)')
+
+    y = np.convolve(w/w.sum(),s,mode='valid')
+    return y[(window_len/2-1):-(window_len/2)]
