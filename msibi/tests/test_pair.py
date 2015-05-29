ef67d57c8d59bf0b3fdfc78c802a572e4c3f5415
Tim Moore <tcmoore3@gmail.com>
2015-05-29 17:28:34 -0500

    Fix unit tests for Pair class

--- msibi/tests/test_pair.py
+++ msibi/tests/test_pair.py
@@ -2,58 +2,74 @@ import os
 import pytest
 import tempfile
 
+import mdtraj as md
 import numpy as np
 
 from msibi.potentials import mie
 from msibi.pair import Pair
+from msibi.state import State
 
+
+dr = 0.1/6.0
+r = np.arange(0, 2.5+dr, dr)
+pot_r = np.arange(0, 2.0+dr, dr)
+r_range = np.asarray([0.0, 2.5+dr])
+n_bins = 151
 
 @pytest.mark.skipif(True, reason='This function used for setting up tests')
 def init_state():
     pair = Pair('0', '1', potential=mie(r, 1.0, 1.0))
-    t = md.load('./state/traj.dcd', top='./state/sys.hoomdxml')
+    t = md.load('./state/query.dcd', top='./state/sys.hoomdxml')
     pair_list = t.top.select_pairs('name "0"', 'name "1"')
+    rdf = np.loadtxt('./state/rdf.txt')
     alpha = 0.5
-    state = (1.987e-3, 305.0, state_dir='./state/',
-             traj_file='./state/traj.dcd', top_file='./state/sys.hoomdxml',
-             name='state0')
+    state = State(1.987e-3, 305.0, state_dir='./state/', 
+            top_file='sys.hoomdxml', name='state0')
     pair.add_state(state, rdf, alpha, pair_list)
-    return pair, state
+    return pair, state, rdf
 
 def test_pair_name():
-    pair, state = init_state()
-    assert(pair.name = '0-1')
+    pair, state, rdf = init_state()
+    assert(pair.name == '0-1')
 
 def test_save_table_potential():
-    dr = 0.1
-    r = np.arange(0, 5.0+dr, dr)
     pair = Pair('A', 'B', potential=mie(r, 1.0, 1.0))
     pair.potential_file = tempfile.mkstemp()[1]
     pair.save_table_potential(r, dr)
     assert os.path.isfile(pair.potential_file)
 
 def test_add_state():
-    pair, state = init_state()
-    assert(pair.states[state]['target_rdf'] == rdf)
+    pair, state, rdf = init_state()
+    assert(np.array_equal(pair.states[state]['target_rdf'], rdf))
     assert(pair.states[state]['current_rdf'] == None)
-    assert(pair.states[state]['alpha'] == alpha)
+    assert(pair.states[state]['alpha'] == 0.5)
     assert(len(pair.states[state]['pair_indices']) == 145152)
-    assert(assert(pair.states[state]['f_fit']) == 0)
+    assert(len(pair.states[state]['f_fit']) == 0)
 
 def test_calc_current_rdf_no_smooth():
-    pair, state = init_state()
-    pair.compute_current_rdf(state, [0.0, 2.0], 151, smooth=False, max_frames=1e3)
+    pair, state, rdf = init_state()
+    state.reload_query_trajectory()
+    pair.compute_current_rdf(state, r_range, n_bins+1, smooth=False, max_frames=1e3)
     assert(pair.states[state]['current_rdf']) != None
     assert(len(pair.states[state]['f_fit']) > 0)
 
 def test_calc_current_rdf_smooth():
-    pair, state = init_state()
-    pair.compute_current_rdf(state, [0.0, 2.0], 151, smooth=True, max_frames=1e3)
+    pair, state, rdf = init_state()
+    state.reload_query_trajectory()
+    pair.compute_current_rdf(state, r_range, n_bins+1, smooth=True, max_frames=1e3)
     assert(pair.states[state]['current_rdf']) != None
     assert(len(pair.states[state]['f_fit']) > 0)
 
 def test_save_current_rdf():
-    pair, state = init_state()
-    pair.compute_current_rdf(state, [0.0, 2.0], 151, smooth=True, max_frames=1e3)
+    pair, state, rdf = init_state()
+    state.reload_query_trajectory()
+    pair.compute_current_rdf(state, r_range, n_bins+1, smooth=True, max_frames=1e3)
     pair.save_current_rdf(state, 0, 0.1/6.0)
-    assert os.path.isfile('./rdfs/pair_0-1-state-state-step0.txt')
+    assert os.path.isfile('./rdfs/pair_0-1-state_state0-step0.txt')
+
+def test_update_potential():
+    pair, state, rdf = init_state()
+    state.reload_query_trajectory()
+    pair.compute_current_rdf(state, r_range, n_bins+1, smooth=True, max_frames=1e3)
+    pair.update_potential(np.arange(0, 2.5+dr, dr), r_switch=1.8)
+    assert(not np.array_equal(pair.potential, pair.previous_potential))
