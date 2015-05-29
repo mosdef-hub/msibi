1c22ac15642896d1e4480564630ad3a9eadecd23
Tim Moore <tcmoore3@gmail.com>
2015-05-29 18:37:46 -0500

    Unit tests for utils

---
+++ msibi/tests/test_utils.py
@@ -0,0 +1,34 @@
+import pytest
+
+import numpy as np
+
+from msibi.utils.error_calculation import calc_similarity
+from msibi.utils.general import find_nearest
+from msibi.utils.smoothing import savitzky_golay
+
+
+def test_calc_similarity():
+    a = np.arange(0.0, 5.0, 0.05)
+    b = np.arange(0.0, 5.0, 0.05)
+    assert(calc_similarity(a, b) == 1.0)
+    b *= -1
+    assert(calc_similarity(a, b) == 0.0)
+
+def test_find_nearest():
+    a = np.asarray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    idx, nearest = find_nearest(a, 2.1)
+    assert(idx == 2)
+    assert(nearest == 2)
+
+def test_savitzky_golay():
+    x = np.arange(0, 1, 0.01)
+    y = 2 * x + 1
+    y2 = savitzky_golay(y, 3, 1)
+    assert(y.shape == y2.shape)
+    assert(np.allclose(y, y2))
+    y = x**3.0
+    y2 = savitzky_golay(y, 3, 1)
+    assert(calc_similarity(y, y2) > 0.99)
+
+
+
