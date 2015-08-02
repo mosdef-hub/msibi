4b812f94ea8fbe295d274172b199ca8d4431a11c
Tim Moore <tcmoore3@gmail.com>
2015-08-02 16:38:24 -0500

    More pythonic division in pair.compute_current_rdf

--- msibi/pair.py
+++ msibi/pair.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import os
 
 import mdtraj as md
@@ -98,8 +99,7 @@ class Pair(object):
                                     pairs, r_range=r_range / 10, n_bins=n_bins)
             if g_r_all is None:
                 g_r_all = np.zeros_like(g_r)
-            g_r_all += g_r * len(state.traj[first_frame:last_frame]) / (
-                float(state.traj.n_frames))
+            g_r_all += g_r * len(state.traj[first_frame:last_frame]) / state.traj.n_frames
             first_frame = last_frame
         r *= 10
         rdf = np.vstack((r, g_r)).T
