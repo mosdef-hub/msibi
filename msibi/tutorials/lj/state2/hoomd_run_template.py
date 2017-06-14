28527afe75c9e2d82ea4ab470decb693b2562a08
Davy Yue <davy.yue@vanderbilt.edu>
2017-06-14 13:45:39 -0500

    Remove stray comments

--- msibi/tutorials/lj/state2/hoomd_run_template.py
+++ msibi/tutorials/lj/state2/hoomd_run_template.py
@@ -1,13 +1,6 @@
-# **** 2017_05_31 Notes ******
-# update file to correct hoomd 2.0 syntax
-# look up docs
-# combine with state.py in main folder
-
-
 all = hoomd.group.all()
 nvt_int = hoomd.md.integrate.langevin(group=all, kT=T_final, seed=1)
 hoomd.md.integrate.mode_standard(dt=0.001)
-
 
 hoomd.run(1e2)
 output_dcd = hoomd.dump.dcd(filename='query.dcd', period=100, overwrite=True)
