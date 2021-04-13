b6f5854b14c77f3caa15cc3eaad42387399414ea
chrisjones4 <chrisjones4@u.boisestate.edu>
2021-04-13 11:02:55 -0600

    remove license from individual .py files

--- msibi/workers.py
+++ msibi/workers.py
@@ -1,33 +1,3 @@
-##############################################################################
-# MSIBI: A package for optimizing coarse-grained force fields using multistate
-#   iterative Boltzmann inversion.
-# Copyright (c) 2017 Vanderbilt University and the Authors
-#
-# Authors: Christoph Klein, Timothy C. Moore
-# Contributors: Davy Yue
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files, to deal
-# in MSIBI without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# # copies of MSIBI, and to permit persons to whom MSIBI is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of MSIBI.
-#
-# MSIBI IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH MSIBI OR THE USE OR OTHER DEALINGS ALONG WITH
-# MSIBI.
-#
-# You should have received a copy of the MIT license.
-# If not, see <https://opensource.org/licenses/MIT/>.
-##############################################################################
-
 from __future__ import division, print_function
 
 import itertools
