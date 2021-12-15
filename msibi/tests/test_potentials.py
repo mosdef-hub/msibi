75788ba8aa994ccc17e218cef39da734029ca26e
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-12-15 16:28:03 -0700

    Add quick test for both head corrections

--- msibi/tests/test_potentials.py
+++ msibi/tests/test_potentials.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from msibi.potentials import alpha_array, mie, tail_correction
+from msibi.potentials import alpha_array, mie, tail_correction, head_correction
 
 
 def test_tail_correction():
@@ -24,3 +24,21 @@ def test_calc_alpha_array():
     form = "margaret-thatcher"
     with pytest.raises(ValueError):
         alpha = alpha_array(alpha0, r, form)
+
+
+def test_head_correction():
+    dr = 0.05
+    cutoff = 8
+    r = np.arange(0, 2.5, dr)
+    V_prev = mie(r, 1, 1)
+    V = mie(r, 2, 1)
+    V[:cutoff] = np.inf
+
+    linear_V = head_correction(r, V, V_prev, "linear")
+    assert not np.isnan(linear_V).any() and not np.isinf(linear_V).any()
+    assert all(linear_V[cutoff:] == V[cutoff:])
+
+    exp_V = head_correction(r, V, V_prev, "exponential")
+    assert not np.isnan(exp_V).any() and not np.isinf(exp_V).any()
+    assert all(exp_V[cutoff:] == V[cutoff:])
+    assert exp_V > linear_V
