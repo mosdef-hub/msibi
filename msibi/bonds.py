e2ebd714bbf7ce8d7a020ddad2433a814c936bae
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2022-01-26 16:08:49 -0700

    add harmonic table func to Angle class

--- msibi/bonds.py
+++ msibi/bonds.py
@@ -10,7 +10,7 @@ TABLE_ANGLE_ENTRY = ""
 
 
 class Bond(object):
-    """Creates a bond potential, either to be helpd constant, or to be
+    """Creates a bond potential, either to be held constant, or to be
     optimized.
 
     Parameters
@@ -54,7 +54,7 @@ class Bond(object):
         self.bond_init = "harmonic_bond = hoomd.md.bond.harmonic()"
         self.bond_entry = HARMONIC_BOND_ENTRY.format(self.name, self.k, self.r0)
     
-    def set_quadratic(self, l0, k4, k3, k2, l_min, l_max, n_bond_points=101):
+    def set_quadratic(self, l0, k4, k3, k2, l_min, l_max, n_points=101):
         """Set a bond potential based on the following function:
 
             V(l) = k4(l-l0)^4 + k3(l-l0)^3 + k2(l-l0)^2
@@ -75,7 +75,7 @@ class Bond(object):
             The lower bound of the bond potential lengths
         l_max : float, required
             The upper bound of the bond potential lengths
-        n_bond_points : int, default = 101 
+        n_points : int, default = 101 
             The number of points between l_min-l_max used to create
             the table potential
 
@@ -85,10 +85,10 @@ class Bond(object):
         self.k4 = k4
         self.k3 = k3
         self.k2 = k2
-        self.dl = (l_max - l_min) / n_bond_points
+        self.dl = (l_max - l_min) / n_points
         self.l_range = np.arange(l_min, l_max + self.dl, self.dl)
         self.potential = create_bond_table(self.l_range)
-        self.bond_init = f"btable = bond.table(width={n_bond_points})"
+        self.bond_init = f"btable = bond.table(width={n_points})"
         self.bond_entry = TABLE_BOND_ENTRY.format(
                 self.name, self.potential_file
         ) 
@@ -152,6 +152,16 @@ class Bond(object):
 
 
 class Angle(object):
+    """Creates a bond angle potential, either to be held constant, or to be
+    optimized.
+
+    Parameters
+    ----------
+    type1, type2, type3 : str, required
+        The name of each particle type in the bond.
+        Must match the names found in the State's .gsd trajectory file
+
+    """
     def __init__(self, type1, type2, type3):
         self.type1 = type1
         self.type2 = type2
@@ -160,12 +170,62 @@ class Angle(object):
         self._states = dict()
 
     def set_harmonic(self, k, theta0):
+        self.angle_type = "harmonic"
         self.k = k
         self.theta0 = theta0
         self.angle_init = "harmonic_angle = hoomd.md.angle.harmonic()"
         self.angle_entry = HARMONIC_ANGLE_ENTRY.format(
                 self.name, self.k, self.theta0
         ) 
+
+    def set_quadratic(
+            self, theta0, k4, k3, k2, theta_min, theta_max, n_points=101
+    ):
+        """Set a bond potential based on the following function:
+
+            V(theta) = k4(theta-theta0)^4 + k3(theta-theta0)^3 + k2(theta-theta0)^2
+
+        Using this method will create a table potential V(theta) over the range
+        theta_min - theta_max.
+
+        This should be the angle potential form of choice when optimizing angles 
+        as opposed to using `set_harmonic`. However, you can also use this
+        method to set a static angle potential while you are optimizing other
+        potentials such as Bonds or Pairs.
+
+        Parameters
+        ----------
+        theta0, k4, k3, k2 : float, required
+            The paraters used in the V(theta) function described above
+        theta_min : float, required
+            The lower bound of the angle potential angles 
+        theta_max : float, required
+            The upper bound of the angle potential angles
+        n_points : int, default = 101 
+            The number of points between theta_min-theta_max used to create
+            the table potential
+
+        """
+        self.angle_type = "quadratic"
+        self.theta0 = theta0
+        self.k4 = k4
+        self.k3 = k3
+        self.k2 = k2
+        self.dtheta = (theta_max - theta_min) / n_points
+        self.theta_range = np.arange(
+                theta_min, theta_max + self.dtheta, self.dtheta
+        )
+        self.potential = create_angle_table(self.theta_range)
+        self.angle_init = f"atable = angle.table(width={n_points})"
+        self.angle_entry = TABLE_ANGLE_ENTRY.format(
+                self.name, self.potential_file
+        ) 
+
+        def create_angle_table(theta):
+            T = theta - self.theta0
+            V_theta = (self.k4*(T))**4 + (self.k3*(T))**3 + (self.k2*(T))**2
+            return V_theta
+
 
     def _add_state(self, state):
         if state._opt.optimization == "angles":
