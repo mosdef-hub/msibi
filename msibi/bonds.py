5aa9fed116ea5a3f9d0cc9c11d312f83e92b4745
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2022-02-02 14:24:35 -0700

    fix entry str

--- msibi/bonds.py
+++ msibi/bonds.py
@@ -6,9 +6,9 @@ from msibi.utils.error_calculation import calc_similarity
 
 
 HARMONIC_BOND_ENTRY = "haromonic_bond.bond_coeff.set('{}', k={}, r0={})"
-TABLE_BOND_ENTRY = "btable.bond_coeff.set('{}', {})"
+TABLE_BOND_ENTRY = "btable.bond_coeff.set('{}', '{}')"
 HARMONIC_ANGLE_ENTRY = "harmonic_angle.angle_coeff.set('{}', k={}, t0={})"
-TABLE_ANGLE_ENTRY = "atable.angle_coeff.set('{}', {})"
+TABLE_ANGLE_ENTRY = "atable.angle_coeff.set('{}', '{}')"
 
 
 class Bond(object):
