792e63b4476a34f2047f28a117131937ed9d08af
Marjan <marjan@ENG402661.local>
2024-02-27 12:29:12 -0700

    use scipy curve_fit for head and tail correction

--- msibi/potentials.py
+++ msibi/potentials.py
@@ -2,6 +2,7 @@ import warnings
 import os
 
 import numpy as np
+from scipy.optimize import curve_fit
 
 from msibi.utils.general import find_nearest
 
@@ -14,7 +15,7 @@ def quadratic_spring(x, x0, k4, k3, k2):
     Used in creating table potentials for bonded potentials.
 
     """
-    V_x = k4*((x-x0)**4) + k3*((x-x0)**3) + k2*((x-x0)**2)
+    V_x = k4 * ((x - x0) ** 4) + k3 * ((x - x0) ** 3) + k2 * ((x - x0) ** 2)
     return V_x
 
 
@@ -52,12 +53,12 @@ def pair_tail_correction(r, V, r_switch):
     S_r = np.ones_like(r)
     r = r[idx_r_switch:]
     S_r[idx_r_switch:] = (
-        (r_cut ** 2 - r ** 2) ** 2
-        * (r_cut ** 2 + 2 * r ** 2 - 3 * r_switch ** 2)
-        / (r_cut ** 2 - r_switch ** 2) ** 3
+            (r_cut ** 2 - r ** 2) ** 2
+            * (r_cut ** 2 + 2 * r ** 2 - 3 * r_switch ** 2)
+            / (r_cut ** 2 - r_switch ** 2) ** 3
     )
     return V * S_r
-    
+
 
 def pair_head_correction(r, V, previous_V, form="linear"):
     """Apply head correction to V making it go to a finite value at V(0).
@@ -118,6 +119,9 @@ def bond_correction(r, V, form):
     if form == "linear":
         head_correction_function = linear_head_correction
         tail_correction_function = linear_tail_correction
+    elif form == "linear_optimized":
+        head_correction_function = linear_head_correction_optimized
+        tail_correction_function = linear_tail_correction_optimized
     elif form == "exponential":
         head_correction_function = exponential_head_correction
         tail_correction_function = exponential_tail_correction
@@ -133,14 +137,14 @@ def bond_correction(r, V, form):
         for idx, v in enumerate(V[start:end]):
             if not np.isfinite(v):
                 try:
-                    avg = (V[idx+start-1] + V[idx+start+1]) / 2
-                    V[idx+start] = avg
+                    avg = (V[idx + start - 1] + V[idx + start + 1]) / 2
+                    V[idx + start] = avg
                 except IndexError:
                     pass
         # Trim off edge cases
         _real_idx = np.where(np.isfinite(V))[0]
         real_idx = max(
-                [list(g) for g in mit.consecutive_groups(_real_idx)], key=len
+            [list(g) for g in mit.consecutive_groups(_real_idx)], key=len
         )
 
     head_cutoff = real_idx[0] - 1
@@ -149,12 +153,12 @@ def bond_correction(r, V, form):
     head_correction_V = head_correction_function(r=r, V=V, cutoff=head_cutoff)
     # Potential with both head correction and tial correciton applied
     tail_correction_V = tail_correction_function(
-            r=r, V=head_correction_V, cutoff=tail_cutoff
+        r=r, V=head_correction_V, cutoff=tail_cutoff
     )
     return tail_correction_V, real_idx, head_cutoff, tail_cutoff
 
 
-def linear_tail_correction(r, V, cutoff, window=4):
+def linear_tail_correction(r, V, cutoff, window=6):
     """Use a linear function to smoothly force V to a finite value at V(cut).
 
     Parameters
@@ -170,15 +174,42 @@ def linear_tail_correction(r, V, cutoff, window=4):
 
     """
     slope = (
-            V[cutoff - 1] - V[cutoff - window]
+                    V[cutoff - 1] - V[cutoff - window]
             ) / (r[cutoff - 1] - r[cutoff - window])
     if slope < 0:
         slope = -slope
     V[cutoff:] = slope * (r[cutoff:] - r[cutoff - 1]) + V[cutoff - 1]
-    return V 
+    return V
 
 
-def linear_head_correction(r, V, cutoff, window=4):
+def linear_tail_correction_optimized(r, V, cutoff, window=6):
+    """Use a linear function to smoothly force V to a finite value at V(cut).
+
+    This function uses scipy.optimize.curve_fit to find the slope and intercept
+    of the linear function that is used to correct the tail of the potential.
+
+    Parameters
+    ----------
+    r : np.ndarray
+        Separation values
+    V : np.ndarray
+        Potential at each of the separation values
+    cutoff : int
+        The last real value of V when iterating backwards
+    window : int
+        Number of data points backward from cutoff to use in slope calculation
+
+    """
+
+    def linear(x, m, b):
+        return m * x + b
+
+    popt, pcov = curve_fit(linear, r[cutoff - window:cutoff], V[cutoff - window:cutoff])
+    V[cutoff:] = linear(r[cutoff:], *popt)
+    return V
+
+
+def linear_head_correction(r, V, cutoff, window=6):
     """Use a linear function to smoothly force V to a finite value at V(0).
     Parameters
     ----------
@@ -193,11 +224,36 @@ def linear_head_correction(r, V, cutoff, window=4):
 
     """
     slope = (
-            V[cutoff + 1] - V[cutoff + window]
+                    V[cutoff + 1] - V[cutoff + window]
             ) / (r[cutoff + 1] - r[cutoff + window])
     if slope > 0:
         slope = -slope
     V[: cutoff + 1] = slope * (r[: cutoff + 1] - r[cutoff + 1]) + V[cutoff + 1]
+    return V
+
+
+def linear_head_correction_optimized(r, V, cutoff, window=6):
+    """Use a linear function to smoothly force V to a finite value at V(0).
+
+    This function uses scipy.optimize.curve_fit to find the slope and intercept
+    of the linear function that is used to correct the head of the potential.
+
+    Parameters
+    ----------
+    r : np.ndarray
+        Separation values
+    V : np.ndarray
+        Potential at each of the separation values
+    cutoff : int
+        The first real value of V when iterating forwards
+    window : int
+        Number of data points forward from cutoff to use in slope calculation
+    """
+    def linear(x, m, b):
+        return m * x + b
+
+    popt, pcov = curve_fit(linear, r[cutoff + 1:cutoff + window], V[cutoff + 1:cutoff + window])
+    V[:cutoff + 1] = linear(r[:cutoff + 1], *popt)
     return V
 
 
@@ -218,8 +274,8 @@ def exponential_tail_correction(r, V, cutoff):
 
     """
     raise RuntimeError("Exponential tail corrections are not implemented."
-            "Use the linear correction form when optimizing bonds and angles."
-    )
+                       "Use the linear correction form when optimizing bonds and angles."
+                       )
     dr = r[cutoff - 1] - r[cutoff - 2]
     B = np.log(V[cutoff - 1] / V[cutoff - 2]) / dr
     A = V[cutoff - 1] * np.exp(B * r[cutoff - 1])
