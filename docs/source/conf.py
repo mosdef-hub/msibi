e01d127600b285ffc7033d76143f10c311ebd495
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2025-07-15 08:47:47 +0100

    comment out html_static_path in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,4 +34,4 @@ source_suffix = [".rst"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+#html_static_path = ["_static"]
